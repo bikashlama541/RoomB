86fd57cf483b4819671d3dac27efb4a8bcce3566
sushilgnawali9 <sushil.gnawali10@gmail.com>
2019-09-27 23:43:46 -0500

    New python file

---
+++ if_else.py
@@ -0,0 +1,11 @@
+a = 30
+b = 5
+if  a < b:
+     print("a is less than b")
+elif a == b:
+     print("a is equals to b")
+elif a > b + 20:
+     print("a is greater than b by more than 20")
+else:
+     print("a is greater than b")
+
